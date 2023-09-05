fc8d58ab33b88094c5ce21922e76bf1d5d894942
Vivekthapa0998 <Vivek.Thapa@geminisolutions.com>
2023-09-05 19:13:13 +0530

    validation with ?

--- MagicVilla_Web/Models/Dto/VillaDto.cs
+++ MagicVilla_Web/Models/Dto/VillaDto.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace MagicVilla_VillaAPI.Models.Dto
+namespace MagicVilla_Web.Models.Dto
 {
-    public class VillaCreateDto
+    public class VillaDto
     {
-        
+        public int Id { get; set; }
         [Required]
         [MaxLength(30)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int? Occupancy { get; set; }
-        public string? Details { get; set; }
+        public string Details { get; set; }
         [Required]
         public double Rate { get; set; }
         public int? Sqft { get; set; }
